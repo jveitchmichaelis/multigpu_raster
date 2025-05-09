3794442e6397eae9e7d8482edd8fc5eeb5a3d482
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2025-05-09 13:36:41 -0400

    update logging

--- src/multi_gpu_raster/inference.py
+++ src/multi_gpu_raster/inference.py
@@ -79,6 +79,8 @@ def main(cfg: DictConfig):
 
     logging.info(f"Prediction completed in {end_time - start_time:.2f} seconds")
 
+    logging.info(model.gpu_stats)
+
 
 if __name__ == "__main__":
     main()
--- src/multi_gpu_raster/model.py
+++ src/multi_gpu_raster/model.py
@@ -13,6 +13,27 @@ class ResNet50Prediction(pl.LightningModule):
     def forward(self, x):
         return self.model(x)
 
+    def on_predict_batch_end(self, outputs, batch, batch_idx, dataloader_idx=0):
+
+        if torch.cuda.is_available():
+            device = torch.cuda.current_device()
+            memory_allocated = torch.cuda.memory_allocated(device)
+            max_memory_allocated = torch.cuda.max_memory_allocated(device)
+
+            # Update GPU stats
+            self.gpu_stats["max_memory"] = max(
+                self.gpu_stats.get("max_memory", 0), max_memory_allocated
+            )
+            self.gpu_stats["total_memory"] = (
+                self.gpu_stats.get("total_memory", 0) + memory_allocated
+            )
+            self.gpu_stats["iterations"] = self.gpu_stats.get("iterations", 0) + 1
+
+            # Reset peak memory for the next batch
+            torch.cuda.reset_peak_memory_stats(device)
+
+        return super().on_predict_batch_end(outputs, batch, batch_idx, dataloader_idx)
+
 
 class ObjectDetector(pl.LightningModule):
     def __init__(self):
@@ -39,20 +60,6 @@ class ObjectDetector(pl.LightningModule):
                 self.gpu_stats.get("total_memory", 0) + memory_allocated
             )
             self.gpu_stats["iterations"] = self.gpu_stats.get("iterations", 0) + 1
-
-            # Optionally log the stats
-            self.log(
-                "gpu_max_memory",
-                self.gpu_stats["max_memory"],
-                prog_bar=True,
-                on_epoch=True,
-            )
-            self.log(
-                "gpu_avg_memory",
-                self.gpu_stats["total_memory"] / self.gpu_stats["iterations"],
-                prog_bar=True,
-                on_epoch=True,
-            )
 
             # Reset peak memory for the next batch
             torch.cuda.reset_peak_memory_stats(device)
